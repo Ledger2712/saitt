5fbdc71dc9d73e1520708df6284af4e74fef2ae5
Ledger2712 <denis27122004@mail.ru>
2025-05-15 19:47:15 +0300

    Initial commit

--- sait-main/sait/settings.py
+++ sait-main/sait/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'templates'
+    BASE_DIR / 'static'
 ]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
